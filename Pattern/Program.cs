80026d1807227e0d73a952513ddb8807772b0050
Caitlin Murnick <caitlinmurnick@gmail.com>
2018-01-20 09:34:25 -0600

    got working

--- Pattern/Program.cs
+++ Pattern/Program.cs
@@ -10,6 +10,30 @@ namespace Pattern
     {
         static void Main(string[] args)
         {
-        }
+        
+                var output = string.Empty;
+                var input = Console.ReadLine();
+
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    var letter = input[i].ToString();
+
+                    output += letter.ToUpper();
+                    for (int j = 0; j < i; j++)
+                    {
+                        output += letter.ToLower();
+                    }
+
+                    if (i < input.Length - 1)
+                    {
+                        output += "-";
+                    }
+                }
+
+                Console.WriteLine(output);
+                Console.ReadLine();
+            }
+        
     }
 }
